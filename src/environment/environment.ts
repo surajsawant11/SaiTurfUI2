dc4edfaed84dda389181791d2f43051746d22b57
surajsawant11 <srjsawant115@gmail.com>
2025-02-21 21:28:33 +0530

    update tacke from dev and commit

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api', // Add the apiUrl here
+    apiUrl: 'http://localhost:8080/api', // Add the apiUrl here
 };
