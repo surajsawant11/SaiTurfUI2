c6ea1241cd71dd91b0b542ff4e02aa878583b8a2
suraj office <srjsawant115@gmail.com>
2025-02-24 17:35:41 +0530

    adde slider for card and view all button

--- src/app/features/dashboard/home/home.component.ts
+++ src/app/features/dashboard/home/home.component.ts
@@ -1,19 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ElementRef, HostListener} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { loadHome } from './store/home.actions';
 import { selectHomeData } from './store/home.selectors';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,SlickCarouselModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements AfterViewInit {
   posts$: Observable<any[]>; // Observable to hold posts
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+  private isDragging = false;
 
   constructor(private store: Store) {
     this.posts$ = this.store.select(selectHomeData); // Select posts from the store
@@ -23,6 +26,7 @@ export class HomeComponent implements AfterViewInit {
     this.store.dispatch(loadHome());
   }
   ngAfterViewInit() {
+    // this.updateScrollIndicator();
   }
 
   // Data for the swiper slides
@@ -31,8 +35,6 @@ export class HomeComponent implements AfterViewInit {
     { image: 'assets/images/img6.jpg', alt: 'Slide 2' },
     { image: 'assets/images/img8.jpg', alt: 'Slide 3' },
   ];
-
-
 
 
   // Data for available turfs
@@ -58,6 +60,27 @@ export class HomeComponent implements AfterViewInit {
       price: '$40 per hour',
       image: 'assets/images/img8.jpg',
     },
+    {
+      id:'1',
+      name: 'Football Turf',
+      description: 'Perfect for 5-a-side games. Book your slot today!',
+      price: '$50 per hour',
+      image: 'assets/images/img7.jpg',
+    },
+    {
+      id:'2',
+      name: 'Cricket Turf',
+      description: 'Ideal for cricket games with your friends or team.',
+      price: '$60 per hour',
+      image: 'assets/images/img5.jpg',
+    },
+    {
+      id:'3',
+      name: 'Indoor Turf',
+      description: 'A great place for indoor football or futsal.',
+      price: '$40 per hour',
+      image: 'assets/images/img8.jpg',
+    }
     // Add more turfs as needed
   ];
 
@@ -76,5 +99,11 @@ export class HomeComponent implements AfterViewInit {
     // Add more testimonials as needed
   ];
 
-
+  scrollLeft() {
+    this.scrollContainer.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+  
+  scrollRight() {
+    this.scrollContainer.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
 }
